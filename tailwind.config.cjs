631fda039b282947004d9b70b6950c60ceca18d8
Achufam24 <achuulimagbama@gmail.com>
2022-08-16 17:43:04 +0100

    Add existing project files

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'linearbg': '#3f3cbb',
+      'textfieldbg': '#161A24',
+      'custom-grey': 'rgba(246,246,246,0.7)',
+      'inactiveTextField': 'rgba(140, 144, 153, 1)',
+      'primary-c': '#fb923c',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
+  plugins: [],
+}
